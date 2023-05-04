297974179782f889d290ccbd16cfdcf91e03cce0
alu0101130675 <50322766+alu0101130675@users.noreply.github.com>
2023-05-04 10:46:58 +0100

    Update questions.ts

--- src/store/questions.ts
+++ src/store/questions.ts
@@ -15,7 +15,7 @@ export const useQuestionsStore = create<State>()(persist((set, get) => {
     questions: [],
     currentQuestion: 0,
     fetchQuestions: async (limit: number) => {
-      const res = await fetch('http://localhost:5173/data.json')
+      const res = await fetch('https://js-examplequizz.netlify.app/data.json')
       const json = await res.json()
       const questions = json.sort(() => Math.random() - 0.5).slice(0, limit)
       set({ questions })
